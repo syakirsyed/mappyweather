644db695e46ee033e727fee8b55c0ac7b35450c9
syakirsyed <42580343+syakirsyed@users.noreply.github.com>
2020-02-23 00:56:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function getCountryName(c) {
 }
 
 function onMapClickAgain() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
