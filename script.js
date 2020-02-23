afce0db78f023d83bdf01c4ff0a059b815ef9fea
syakirsyed <syakir.syed@gmail.com>
2020-02-23 00:53:40 +0000

    sat

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let mymap = L.map("mapid").setView([51.505, -0.09], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
@@ -15,7 +14,7 @@ let lon = [];
 
 function onMapClick(e) {
     popup.setLatLng(e.latlng)
-         .setContent(e.latlng.toString())
+         .setContent()
          .openOn(mymap);
     lat = e.latlng.lat;
     lon = e.latlng.lng;
@@ -72,9 +71,69 @@ function weatherChecker(w) {
     }
 }
 
+function weatherChecker2(w) {
+    if ( w === "01d" ) {
+        return "clear-day";
+    } else if ( w === "01n") {
+        return "clear-night"
+    } else if ( w === "02d" || w === "03d" || w === "04d" || w === "09d" || w === "10d" || w === "11d" || w === "13d" ) {
+        return "cloudy-day";
+    } else if ( w === "02n" || w === "03n" || w === "04n" || w === "09n" || w === "10n" || w === "11n" || w === "13n") {
+        return "cloudy-night";
+    } else {
+        return "question-mark"
+    }
+}
+
+
+function getWindSpeed(speed) {
+    return Math.round((speed * 60 * 60)/1000);
+}
+
+function getWindDescription(s) {
+    if (s < 2 ) {
+        return "Calm winds";
+    } else if (s >= 2 && s < 6) {
+        return "Light air";
+    } else if ( s >= 6 && s < 12 ) {
+        return "Light breeze";
+    } else if ( s >= 12 && s < 20 ) {
+        return "Gentle breeze";
+    } else if ( s >= 20 && s < 29 ) {
+        return "Moderate breeze";
+    } else if ( s >= 29 && s < 39 ) {
+        return "Fresh breeze";
+    } else if ( s >= 39 && s < 50 ) {
+        return "Strong breeze";
+    } else if ( s >= 50 && s < 62 ) {
+        return "Moderate gale";
+    } else if ( s >= 62 && s < 75 ) {
+        return "Fresh gale";
+    } else if ( s >= 75 && s < 89 ) {
+        return "Severe gale";
+    } else if ( s >= 89 && s < 103 ) {
+        return  "Storm";
+    } else if ( s >= 103 && s < 118 ) {
+        return "Violent storm";
+    } else {
+        return "Hurricane";
+    }
+}
+
 const apiKey = "7445ce21c975bc68f969abfe8f056166";
 
 let weatherInfo = [];
+
+let orgCountry = [];
+
+function getCountryName(c) {
+    fetch(`https://restcountries.eu/rest/v2/alpha/${c}`)
+    .then(response => response.json())
+    .then(data => {
+        orgCountry.shift();
+        orgCountry.push(data);
+    });
+}
 
 function onMapClickAgain() {
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
@@ -90,23 +149,34 @@ function onMapClickAgain() {
             let currentHumidity = weatherInfo[0].main.humidity + "%";
             let extractTime = weatherInfo[0].dt + weatherInfo[0].timezone;
             let currentTime = convertTime(extractTime);
-            let currentWind = weatherInfo[0].wind.speed + "m/s";
-            let backCol = randomColor();
-            let currentIcon = weatherInfo[0].weather[0].main;
-            let dayNight = dayOrNight(weatherInfo[0].weather[0].icon);
-            let newIcon = weatherChecker(weatherInfo[0].weather[0].icon);
+            let currentWind = getWindSpeed(weatherInfo[0].wind.speed) + " km/h";
+            let windDescription = getWindDescription(getWindSpeed(weatherInfo[0].wind.speed));
             
+            let currentIcon = weatherInfo[0].weather[0].main;
+            let dayNight = weatherChecker2(weatherInfo[0].weather[0].icon);
+            let backCol = dayNight;
+
+            let newIcon = weatherChecker(weatherInfo[0].weather[0].icon);
+
+            let lengthOfDay = convertTime(weatherInfo[0].sys.sunset - weatherInfo[0].sys.sunrise);
+
             let newLocation = document.createElement("div");
             newLocation.setAttribute("id", "weatherCards");
-            newLocation.setAttribute("style", `background-color: ${backCol};`);
+            newLocation.setAttribute("class", `${backCol}`);
             let markup = `
-            <h2 id="location">${currentLocation}</h2>
-            <p id="temperature">${currentTemp}</p>
-            <div id=${currentIcon}><i class="fas fa-${newIcon} fa-3x"></i></div>
-            <p>${currentDescription}</p>
-            <div id="humidity"><i class="fas fa-tint"></i><p>${currentHumidity}</p></div>
-            <p>${currentTime} --- ${dayNight}</p>
-            <p>${currentWind}</p>
+            <span id="close" onClick="this.parentNode.parentNode.removeChild(this.parentNode);"><i class="fas fa-times"></i></span>
+            <p id="location">${currentLocation}</p>
+            <div id="timeOfDay">
+            <p>${currentTime}</p></div>
+            <div id="temp-icon">
+            <div class="icon" id=${currentIcon}><i class="fas fa-${newIcon} fa-3x"></i></div>
+            <p id="temperature">${currentTemp}</p></div>
+            <p id="description">${currentDescription}</p>
+            <div id="extrainfo">
+            <div id="dayLength"><p class="descriptors">Length of day:</p><p>${lengthOfDay}</p></div>
+            <div id="humidity"><p class="descriptors">Humidity:</p><p>${currentHumidity}</p></div>
+            <div id="wind"><p class="descriptors">Wind:</p><p>${windDescription}</p><p>${currentWind}</p></div>
+            </div>
             `;
             newLocation.innerHTML = markup;
             document.getElementById("weather").appendChild(newLocation);
@@ -117,4 +187,3 @@ function onMapClickAgain() {
 }
 
 mymap.on('click', onMapClickAgain);
-
